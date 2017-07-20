78726bdd8f556234aeb92d1b45a405ba64d824ae
Zeff Morgan <zmorgan@lexmark.com>
2017-07-20 07:49:38 -0400

    First commit. Added a little flesh to README and loaded a basic script for CSV column transposition.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # data-butler
-Various data manipulation scripts
+Small repo to start collecting my various data collection and manipulation scripts.
+
+Everything in here should be pretty straightforward, nothing you couldn't do with other tools, but specific to some of my use cases. For example, in writing a lot of test config files for API performance testing with JMeter, I found it useful to have a script that will take a single column CSV and transform it into a multicolumn, multirow CSV to facilitate building JSON payloads of various sizes.
+
+I will be adding in my other scripts as I have time to revisit them and can insure there are no IP claims from any previous employers.
---
+++ dat_belt.rb
@@ -0,0 +1,54 @@
+require 'optparse'
+require 'ostruct'
+require 'csv'
+
+options = OpenStruct.new
+options.output = 'my.csv'
+options.cols = 10
+options.prefix = 'var'
+
+OptionParser.new do |opt|
+  opt.on('-v', '--version', 'Returns current version of this utility') { |o| options[:version] = true }
+  opt.on('-f', '--file FILE_PATH', 'Path to data file to be processed.') { |o| options[:file] = o }
+  opt.on('-o', '--ouput FILE_PATH', 'Path to data file to be written.') { |o| options[:output] = o }
+  opt.on('-c', '--cols NUMBER_OF_COLUMNS', 'Number of columns to break on.') { |o| options[:cols] = o }
+  opt.on('-p', '--prefix PREFIX', 'Custom variable prefix.') { |o| options[:prefix] = o }
+end.parse!
+
+case
+when options[:version] == true
+  puts "version 0.1"
+  return
+when !options[:file]
+  puts "Please run the command with the -f option, specifying an input CSV."
+  return
+end
+
+CSV.open(options[:output], "wb") do |csv|
+  headers = []
+  (1..(options[:cols].to_i)).each do |i|
+    headers.push(options[:prefix] + i.to_s.rjust(options[:cols].to_s.length, "0"))
+  end
+
+  csv << headers
+
+  File.open(options[:file]) do |f|
+    row = []
+    f.each_line.with_index do |line, idx|
+      row.push(line.chomp.to_i)
+      if (idx+1)%options[:cols].to_i == 0
+        csv << row
+        row.clear
+      end
+    end
+  end
+end
+
+
+# TODO code as OO vs this procedural prototype
+# TODO tc Transposes the comma delimited data in a single row to a single column.
+# TODO tr Transposes the comma delimited data in a single column to rows of N length.
+# TODO cr Returns the number of rows in a set.
+# TODO cc Returns the number of columns per row in a set.
+# TODO mc Merges columns from separate files to a single file with all columns.
+# TODO mr Merges columns from separate files to a single file with all columns.
